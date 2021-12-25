8ceb34b711662209c0a3d258e2e80daef32aa65e
Adam Niedzielski <adamsunday@gmail.com>
2021-12-25 16:07:56 +0100

    Finish form with mocked out worker

--- app/controllers/metric_increases_controller.rb
+++ app/controllers/metric_increases_controller.rb
@@ -1,18 +1,23 @@
 class MetricIncreasesController < ApplicationController
   def new
+    @metric_increase = MetricIncrease.new
   end
 
   def create
-    Transaction.new do
-      metric = Metric.find_or_create_by!(name: metric_increase_params.fetch(:name))
+    metric_increase = MetricIncrease.new(metric_increase_params)
+
+    Metric.transaction do
+      metric = Metric.find_or_create_by!(name: metric_increase.name)
       IncreaseMetricWorker.perform_async(metric.id)
-      raise ActiveRecord::Rollback if metric_increase_params.fetch(:fail)
+      raise ActiveRecord::Rollback if metric_increase.fail_operation?
     end
+
+    redirect_to metrics_url
   end
 
   private
 
   def metric_increase_params
-    params.require(:metric_increase).permit(:name, :fail)
+    params.require(:metric_increase).permit(:name, :fail_operation)
   end
 end
